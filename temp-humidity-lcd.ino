dc0953151bcef6c4d78e2d6774bec1696224de2b
Humberto Daniel <gamekik92@gmail.com>
2025-05-19 08:30:24 -0600

    feat: add lcd with sensor DHT11

--- temp-humidity-lcd.ino
+++ temp-humidity-lcd.ino
@@ -1,15 +1,24 @@
 #include <LiquidCrystal.h>
+#include <DHT.h>
+
+#define DHT_PIN 6
+#define DHT_TYPE DHT11  
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("Hello world");
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float t = dht.readTemperature();
 
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: " + String(t) + " C");
+
+  delay(2000);
 }
